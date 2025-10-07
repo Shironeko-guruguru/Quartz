26403ecab28895eb76b96c337753dfcc9b5e8adb
Shironeko-guruguru <emavue953@gmail.com>
2025-10-07 11:44:19 +0900

    button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// DOM要素を取得
+const trigger = document.querySelector('.pie-menu__trigger');
+const menu = document.querySelector('.pie-menu');
 
+// トリガーボタンがクリックされた時の処理
+trigger.addEventListener('click', () => {
+    // .pie-menu要素に 'is-open' クラスを付け外しする
+    menu.classList.toggle('is-open');
+});
