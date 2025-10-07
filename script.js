92793d04d5fcede9a1514e79b849a3a3b92faf9c
Shironeko-guruguru <emavue953@gmail.com>
2025-10-07 16:42:43 +0900

    1

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ trigger.addEventListener('click', () => {
     // .pie-menu要素に 'is-open' クラスを付け外しする
     menu.classList.toggle('is-open');
 });
+
+const menuitem = document.querySelectorAll('.pie-menu__button');
+const pages = document.querySelectorAll('.page');
+function showPage(pageId) {
+  pages.forEach(page => page.classList.remove('active'));
+  navButtons.forEach(button => button.classList.remove('active'));
+  document.getElementById(pageId).classList.add('active');
+  document.querySelector(`.nav-button[data-page="${pageId}"]`).classList.add('active');
+}
+  menuitem.forEach(button => {
+  button.addEventListener('click', () => {
+    showPage(button.dataset.page);
+  });
+});
+
+async function initializeGame() {
+    showPage('home-page');
+}
+
+initializeGame();
